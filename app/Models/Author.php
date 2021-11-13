659fb81cd17d52c56c0bb31e56664308b59996e0
kabadrian <kabadrian2@gmail.com>
2021-11-13 18:17:49 +0100

    Add author model

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -5,11 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Book extends Model
+class Author extends Model
 {
     use HasFactory;
 
-    public function author(){
-        return $this->belongsTo(Author::class);
+    public function books(){
+        return $this->hasMany(Book::class);
     }
 }
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,39 @@
+
+<footer>
+    <div class="row mt-5 py-3 mx-0" style="background-color: #111417">
+        <div class="col-3 col-md-2">
+            <a class="navbar-brand" href="#">
+                <img src="../xkabac_xzalabaij_project/wtech-project/public/img/whitelogo3.png" alt="BookStore logo" class="logo">
+            </a>
+        </div>
+        <div class="col-9 col-md-10">
+            <div class="row">
+                <nav class="col-12 row">
+                    <div class="col-6 col-sm-3">
+                        <a href="#">
+                            Najčastejšie kladené otázky
+                        </a>
+                    </div>
+                    <div class="col-6 col-sm-3">
+                        <a href="#">
+                            Kontakt
+                        </a>
+                    </div>
+                    <div class="col-6 col-sm-3">
+                        <a href="#">
+                            Obchodné podmienky
+                        </a>
+                    </div>
+                    <div class="col-6 col-sm-3">
+                        <a href="#">
+                            Doprava a platba
+                        </a>
+                    </div>
+                </nav>
+                <div class="col-12 mt-2" style="color: white">
+                    Copyright (c) 2021 Kabi - Zali s.r.o.
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
